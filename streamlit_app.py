1094534a5e44e4504043612f217e7cc861c3ad27
Sanjana <97579455+kitty57@users.noreply.github.com>
2024-02-21 11:15:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.markdown(
     """
     <style>
         *{
-            background: linear-gradient(135deg, #F97794 10%, #623AA2 100%) !important;
+            background: linear-gradient(135deg, #F97794 10%, #623AA2 50%) !important;
         }
 
         .title {
