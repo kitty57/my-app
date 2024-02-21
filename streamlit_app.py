fd8bfdaa56ef84709ee7c58af7baf98d5cc5f95a
Sanjana <97579455+kitty57@users.noreply.github.com>
2024-02-21 11:01:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,4 +89,4 @@ elif navigation == 'Genres and Books':
         st.markdown(f'<p class="genre-header">{genre}</p>', unsafe_allow_html=True)
         for book, img in zip(books, book_img):
             st.markdown(f'<p class="book-name">{book}</p>', unsafe_allow_html=True)
-            st.image(img, use_column_width=True, caption=book)
+            st.image(img, width=200, caption=book)
